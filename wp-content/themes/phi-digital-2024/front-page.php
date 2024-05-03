163b949a8e82d14add221159bf7d2f3a2ed57775
phippr <james.phillips@outlook.com>
2024-05-03 23:17:42 +1000

    logo transition working

--- wp-content/themes/phi-digital-2024/front-page.php
+++ wp-content/themes/phi-digital-2024/front-page.php
@@ -2,11 +2,10 @@
 get_header();
 ?>
 <main id="primary" class="site-main">
-
-	<section id="home-hero" class="hero blue">
+	<section id="home-hero" class="hero blue section-dark">
 		<div id="hero-container" class="fade-in-bottom">
 			<div id="hero-header">
-				<h1>We Create</h1>
+				<h1>We create</h1>
 				<div id="hero-dropdown">
 					<h1 id="hero-dropdown-selected">&nbsp</h1>
 					<div id="hero-dropdown-caret-down" class="shake-vertical"><?php include get_stylesheet_directory() . '/svg/caret-down.svg'; ?></div>
@@ -22,7 +21,10 @@ get_header();
 						</li>
 					</ul>
 				</div>
-				<h1>Websites</h1>
+				<h1>websites</h1>
+			</div>
+			<div id="hero-body">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 			</div>
 		</div>
 		<div id="hero-scroll" class="fade-in-bottom">
@@ -32,11 +34,8 @@ get_header();
 		</div>
 	</section>
 	<section id="recent-work" class="" style="height:800px"></section>
-	<section id="services" class="" style="height:800px"></section>
+	<section id="services" class="blue section-dark" style="height:800px"></section>
 	<section id="get-in-touch" class="" style="height:800px"></section>
-
-
 </main><!-- #main -->
-
 <?php
 get_footer();
--- wp-content/themes/phi-digital-2024/js/custom.js
+++ wp-content/themes/phi-digital-2024/js/custom.js
@@ -27,9 +27,10 @@ jQuery(document).ready(function ($) {
     setTimeout(function () {
       $("#hero-dropdown")
         .css("visibility", "visible")
+        .css("margin", "0 0.6rem")
         .animate(
           {
-            width: finalWidth * 1.05,
+            width: finalWidth * 1.06,
           },
           800,
           function () {
@@ -46,8 +47,6 @@ jQuery(document).ready(function ($) {
             );
           }
         );
-
-      //$("#hero-dropdown-selections").width();
     }, 700);
   }, 200);
 
@@ -57,6 +56,7 @@ jQuery(document).ready(function ($) {
 
   // Toggle slide animation when #hero-dropdown is clicked
   dropdown.click(function () {
+    $(this).addClass("clicked");
     if (selections.is(":visible")) {
       selections.slideUp();
     } else {
@@ -66,8 +66,123 @@ jQuery(document).ready(function ($) {
 
   // Handle click on <li> element within #hero-dropdown-selections
   selections.on("click", "li", function () {
-    var clickedText = $(this).text(); // Get the text content of the clicked <li> element
-    selected.text(clickedText); // Replace the text content of #hero-dropdown-selected
-    selections.slideUp(); // Slide up #hero-dropdown-selections after selection
+    var clickedText = $(this).text();
+
+    selected.fadeOut(300, function () {
+      selected.text(clickedText);
+      selected.fadeIn(300);
+    });
+
+    selections.slideUp();
+  });
+
+  $(document).click(function (event) {
+    // Check if the clicked element is not within the dropdown area
+    if (
+      !dropdown.is(event.target) &&
+      !selections.is(event.target) &&
+      dropdown.has(event.target).length === 0 &&
+      selections.has(event.target).length === 0
+    ) {
+      // Slide up #hero-dropdown-selections if visible
+      if (selections.is(":visible")) {
+        selections.slideUp();
+      }
+    }
+  });
+
+  var $body = $("body");
+  var $logoMain = $(".logo-main");
+  var $logoMainMask = $("#logo-main-mask rect");
+  var $logoAltMask = $("#logo-alt-mask rect");
+  var logoDimensions = {
+    width: $logoMain.outerWidth(),
+    height: $logoMain.outerHeight(),
+  };
+  var scale = $logoMain.outerHeight() / 80;
+  var $sections = $("section");
+  var sections = generateSectionsMap();
+  var maskCache = {};
+
+  function setLogoMasks(isDark, amount) {
+    if (maskCache.isDark === isDark && maskCache.amount === amount) {
+      return;
+    }
+
+    var alt = {};
+    var main = {};
+
+    if (isDark) {
+      alt.y = amount / scale;
+      alt.height = logoDimensions.height / scale - amount;
+
+      main.y = 0;
+      main.height = amount / scale;
+    } else {
+      alt.y = 0;
+      alt.height = amount / scale;
+
+      main.y = amount / scale;
+      main.height = logoDimensions.height / scale - amount;
+    }
+
+    $logoAltMask.attr({
+      y: alt.y - 1,
+      height: alt.height + 1,
+    });
+
+    $logoMainMask.attr({
+      y: main.y,
+      height: main.height,
+    });
+
+    maskCache.isDark = isDark;
+    maskCache.amount = amount;
+  }
+
+  function generateSectionsMap() {
+    var sections = [];
+
+    $sections.each(function () {
+      var top = $(this).offset().top;
+
+      sections.push({
+        $el: this,
+        start: top,
+        end: top + $(this).outerHeight(),
+        isDark: $(this).hasClass("section-dark"),
+      });
+    });
+
+    return sections;
+  }
+
+  function updateLogo(sections) {
+    var scrollOffset = $body.scrollTop();
+    var logoStart = $logoMain.offset().top;
+    var logoEnd = logoStart + logoDimensions.height;
+    var section;
+
+    $.each(sections, function (index, section) {
+      if (section.end >= logoStart) {
+        if (
+          section.end <= logoEnd &&
+          sections[index + 1] &&
+          sections[index + 1].isDark !== section.isDark
+        ) {
+          setLogoMasks(section.isDark, section.end - logoStart);
+        } else {
+          setLogoMasks(section.isDark, logoDimensions.height);
+        }
+
+        return false;
+      }
+    });
+  }
+
+  updateLogo(sections);
+
+  $(window).on("scroll", function () {
+    updateLogo(sections);
   });
 });
